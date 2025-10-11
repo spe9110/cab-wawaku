8fd991ccab340ed46e0d053dcd4dd926f5ba3d4f
Spencer WAWAKU <spencerm365@outlook.fr>
2025-10-11 14:42:44 +0200

    add the concurrently success

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
  
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline text-blue-500 text-center">
       Hello world!
     </h1>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "375px",     // petits téléphones (iPhone SE)
+        sm: "480px",     // mobiles larges
+        md: "768px",     // tablettes
+        lg: "1024px",    // laptops
+        xl: "1280px",    // desktops
+        "2xl": "1440px", // grands laptops (MacBook Air/Pro)
+      },
+    },
   },
   plugins: [],
 }
