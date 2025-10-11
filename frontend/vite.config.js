d226e6142531f0d2679a3a07fcc4bf6dde3d231e
Spencer WAWAKU <spencerm365@outlook.fr>
2025-10-11 15:45:45 +0200

    add the dockerization success and db success connection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    proxy: {
+      '/api/v1': {
+        target: 'http://backend:4000', // use service name, not localhost
+        changeOrigin: true,
+      },
+    },
+  },
 })
