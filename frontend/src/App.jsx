3bb54bdb6a3a0cfe00ba6890831e468afa974974
Spencer WAWAKU <spencerm365@outlook.fr>
2025-10-11 11:35:54 +0200

    frontend config

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+
+
+function App() {
+ 
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
+
+// npm create vite@latest
+// npm install -D tailwindcss@3 postcss autoprefixer
+// npx tailwindcss init -p
+// npm i react-router-dom@
+// npm i react-icons
+// npm i react-toastify
+// npm i react-error-boundary
+// npm i @reduxjs/toolkit react-redux
+// npm install react-intersection-observer
+// npm i axios
+// npm install react-slick --save
+// npm install slick-carousel --save
+// npm i react-hook-form @hookform/resolvers yup
+// npm i react-phone-number-input
+// npm i react-datepicker
+// npm install --save @sentry/react
